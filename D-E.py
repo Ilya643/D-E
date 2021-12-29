6355fd80b09d347a9b61cd10f89583b8a4d169d8
admin <potemkinilya47@gmail.com>
2021-12-29 18:08:18 +0300

    Заготовка меню

--- D-E.py
+++ D-E.py
@@ -3,9 +3,9 @@ import pygame
 pygame.init()
 size = 700, 700
 indent = 70
-part = 15
+part = 20
 screen = pygame.display.set_mode(size)
-font = pygame.font.Font(None, 20)
+font = pygame.font.Font(None, 50)
 
 
 
@@ -42,7 +42,7 @@ class Board:
                                      (indent + x * self.cell_size, indent + y * self.cell_size,
                                       self.cell_size, self.cell_size), 0)
 
-        pygame.draw.rect(screen, pygame.Color("saddlebrown"), (indent - 4, indent - 4,
+        pygame.draw.rect(screen, pygame.Color(0, 0, 0), (indent - 4, indent - 4,
                                                         self.cell_size * 8 + 8,
                                                         self.cell_size * 8 + 8), 4)
         pygame.draw.rect(screen, pygame.Color("gold"), (indent - part - 4, indent - part - 4,
@@ -50,17 +50,55 @@ class Board:
                                                         self.cell_size * 8 + part * 2 + 8), part)
 
 
+class Meny:
+    def __init__(self):
+        self._option_surfaces = []
+        self._callbacks = []
+        self._current_option_index = 0
 
+    def append_option(self, option, callback):
+        self._option_surfaces.append(font.render(option, True, (255, 255, 255)))
+        self._callbacks.append(callback)
+
+    def switch(self, direction):
+        self._current_option_index = max(0, min(self._current_option_index + direction, len(self._option_surfaces) - 1))
+
+    def select(self):
+        self._callbacks[self._current_option_index]()
+
+    def draw(self, surf, x, y, option_y_padding):
+        for i, option in enumerate(self._option_surfaces):
+            option_rect = option.get_rect()
+            option_rect.topleft = (x, y + i * option_y_padding)
+            if i == self._current_option_index:
+                pygame.draw.rect(surf, (0, 100, 0), option_rect)
+            surf.blit(option, option_rect)
+
+
+
+meny = Meny()
 board = Board(8, 8)
+meny.append_option('ggggggggg', lambda: print(1))
+meny.append_option('hhhhhhhhh', quit)
+
+
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w:
+                meny.switch(-1)
+            elif event.key == pygame.K_s:
+                meny.switch(1)
+            elif event.key == pygame.K_SPACE:
+                meny.select()
 
     screen.fill((163, 110, 255))
-    board.render()
+    meny.draw(screen, 100, 100, 75)
+    # board.render()
     pygame.display.flip()
 
 pygame.quit()

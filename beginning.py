1ed60efe96d7d0085824dfbb86bac99978217510
Надя <nad20060628@gmail.com>
2021-12-30 15:20:56 +0300

    меню level up

--- beginning.py
+++ beginning.py
@@ -1,64 +1,39 @@
-from pygame import *
-
-init()
-
-size = (800, 600)
-screen = display.set_mode(size)
-
-ARIAL_30 = font.SysFont('arial', 50)
+import pygame
 
 
-class Menu:
-    def __init__(self):
-        self._options = []
-        self._callbacks = []
-        self._current_option_index = 0
+pygame.init()
+screen = pygame.display.set_mode((500, 400))
+pygame.display.set_caption("menu")
+menuAtivo = True
 
-    def append_option(self, option, callback):
-        self._options.append(ARIAL_30.render(option, True, (255, 255, 255)))
-        self._callbacks.append(callback)
+font = pygame.font.SysFont('Arial', 35)
+# создание кнопок меню
+start_button = pygame.draw.rect(screen, (57, 255, 20), (20, 40, 150, 50))
+screen.blit(font.render('START', True, (0, 0, 0)), (50, 50))
 
-    def switch(self, direction):
-        self._current_option_index = max(0, min(self._current_option_index + direction, len(self._options) - 1))
+continue_button = pygame.draw.rect(screen, (57, 255, 20), (20, 130, 150, 50))
+screen.blit(font.render('RULES', True, (0, 0, 0)), (50, 140))
 
-    def select(self):
-        self._callbacks[self._current_option_index]()
+quit_button = pygame.draw.rect(screen, (57, 255, 20), (20, 220, 150, 50))
+screen.blit(font.render('EXIT', True, (0, 0, 0)), (50, 230))
 
-    def draw(self, surf, x, y, option_y_padding):
-        for i, option in enumerate(self._options):
-            option_rect: Rect = option.get_rect()
-            option_rect.topleft = (x, y + i * option_y_padding)
-            if i == self._current_option_index:
-                draw.rect(surf, (0, 100, 0), option_rect)
-            surf.blit(option, option_rect)
+pygame.display.update()
+pygame.display.flip()
 
-
-running = True
+def startGame():
+    screen.fill('white')
+    pygame.display.flip()
 
 
-def quit_game():
-    global running
-    running = False
-
-
-menu = Menu()
-menu.append_option('START', lambda: print('START'))
-menu.append_option('END', quit_game)
-
-while running:
-    for e in event.get():
-        if e.type == QUIT:
-            quit_game()
-        if e.type == KEYDOWN:
-            if e.key == K_w:
-                menu.switch(-1)
-            elif e.key == K_s:
-                menu.switch(1)
-            elif e.key == K_SPACE:
-                menu.select()
-
-    screen.fill((0, 0, 0))
-
-    menu.draw(screen, 100, 100, 75)
-
-    display.flip()
+while menuAtivo:
+    for evento in pygame.event.get():
+        print(evento)
+        if evento.type == pygame.MOUSEBUTTONDOWN:
+            if pygame.mouse.get_pos()[0] >= 150 and pygame.mouse.get_pos()[1] >= 230:
+                if pygame.mouse.get_pos()[0] <= 250 and pygame.mouse.get_pos()[1] <= 280:
+                        pass
+            if pygame.mouse.get_pos()[0] >= 150 and pygame.mouse.get_pos()[1] >= 90:
+                if pygame.mouse.get_pos()[0] <= 250 and pygame.mouse.get_pos()[1] <= 140:
+                        pass
+            if 170 >= pygame.mouse.get_pos()[0] >= 20 and 270 >= pygame.mouse.get_pos()[1] >= 220:
+                pygame.quit()

c20ada0264e4027f14d64096cbdcfc2711a411fb
Надя <nad20060628@gmail.com>
2021-12-30 16:01:00 +0300

    меню level up

--- beginning.py
+++ beginning.py
@@ -20,20 +20,32 @@ screen.blit(font.render('EXIT', True, (0, 0, 0)), (50, 230))
 pygame.display.update()
 pygame.display.flip()
 
+
 def startGame():
     screen.fill('white')
     pygame.display.flip()
+
+
+
+def rules():
+    screen = pygame.display.set_mode((500, 400))
+    screen.fill('black')
+    pygame.display.set_caption("Rules")
+    pygame.display.update()
+    pygame.display.flip()
+    while pygame.event.wait().type != pygame.QUIT:
+        pygame.display.update()
+    pygame.quit()
+
 
 
 while menuAtivo:
     for evento in pygame.event.get():
         print(evento)
         if evento.type == pygame.MOUSEBUTTONDOWN:
-            if pygame.mouse.get_pos()[0] >= 150 and pygame.mouse.get_pos()[1] >= 230:
-                if pygame.mouse.get_pos()[0] <= 250 and pygame.mouse.get_pos()[1] <= 280:
-                        pass
-            if pygame.mouse.get_pos()[0] >= 150 and pygame.mouse.get_pos()[1] >= 90:
-                if pygame.mouse.get_pos()[0] <= 250 and pygame.mouse.get_pos()[1] <= 140:
-                        pass
+            if 170 >= pygame.mouse.get_pos()[0] >= 20 and 90 >= pygame.mouse.get_pos()[1] >= 40:
+                pass
+            if 170 >= pygame.mouse.get_pos()[0] >= 20 and 180 >= pygame.mouse.get_pos()[1] >= 130:
+                rules()
             if 170 >= pygame.mouse.get_pos()[0] >= 20 and 270 >= pygame.mouse.get_pos()[1] >= 220:
                 pygame.quit()

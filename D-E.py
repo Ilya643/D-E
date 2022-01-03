63834765dffe9e556e0402de7d620e7a8bd3501e
admin <potemkinilya47@gmail.com>
2022-01-03 20:49:19 +0300

    Расстановка фигур - 1

--- D-E.py
+++ D-E.py
@@ -14,7 +14,16 @@ units_peek = (700, 0, 900, 700)
 turtle_active = False
 cell_size = 70
 
-board_units = []
+board_units_f = [['cyber_Turtle_frendly.png', (0, 6)], ['cyber_Turtle_frendly.png', (1, 6)],
+                 ['cyber_Turtle_frendly.png', (2, 6)], ['cyber_Turtle_frendly.png', (3, 6)],
+                 ['cyber_Turtle_frendly.png', (4, 6)], ['cyber_Turtle_frendly.png', (5, 6)],
+                 ['cyber_Turtle_frendly.png', (6, 6)], ['cyber_Turtle_frendly.png', (7, 6)],
+                 ['cyber_Turtle_frendly.png', (3, 7)], ['cyber_Turtle_frendly.png', (4, 7)]]
+board_units_e = [['cyber_Turtle_enemy.png', (1, 1)], ['cyber_Turtle_enemy.png', (2, 1)],
+                 ['cyber_Turtle_enemy.png', (3, 1)], ['cyber_Turtle_enemy.png', (4, 1)],
+                 ['cyber_Turtle_enemy.png', (5, 1)], ['cyber_Turtle_enemy.png', (6, 1)],
+                 ['cyber_Turtle_enemy.png', (2, 0)], ['cyber_Turtle_enemy.png', (3, 0)],
+                 ['cyber_Turtle_enemy.png', (4, 0)], ['cyber_Turtle_enemy.png', (5, 0)]]
 
 
 
@@ -59,28 +68,22 @@ class Board:
                                                         cell_size * 8 + part * 2 + 8,
                                                         cell_size * 8 + part * 2 + 8), part)
         pygame.draw.rect(screen, pygame.Color('darkslategray4'),
-                         units_peek)  # отсюда будем перетаскивать фигуры
-
-        """ Ф и г у р а 1 """
-        pygame.draw.rect(screen, pygame.Color('deeppink2'), (700, 70, 900, 220))
-        pygame.draw.rect(screen, pygame.Color('black'), (700, 70, 120, 120), 1)
-        screen.blit(pygame.transform.scale(
-            pygame.image.load('cyber_Turtle_frendly.png').convert_alpha(), (100, 100)), (710, 80))
+                         units_peek)
 
     def check(self, x, y):
         for i in self.slovar_with_coords:
             coords = self.slovar_with_coords.get(i)
             if coords[0] <= x <= coords[0] + coords[2] and coords[1] <= y <= coords[1] + coords[3]:
                 return [True, i]
-        if units_peek[0] <= x <= units_peek[0] + units_peek[2] and units_peek[1]\
-                <= y <= units_peek[1] + units_peek[3]:
-            if turtle_move[0] <= x <= turtle_move[0] + turtle_move[2] and \
-                    turtle_move[1] <= y <= turtle_move[1] + turtle_move[3]:
-                return 'turtle'
         return None
 
     def update(self):
-        for i in board_units:
+        for i in board_units_f:
+            screen.blit(pygame.transform.scale(
+                pygame.image.load(i[0]).convert_alpha(), (60, 60)), (5 + indent + i[1][0] * cell_size,
+                                                                     5 + indent + i[1][1] * cell_size))
+
+        for i in board_units_e:
             screen.blit(pygame.transform.scale(
                 pygame.image.load(i[0]).convert_alpha(), (60, 60)), (5 + indent + i[1][0] * cell_size,
                                                                      5 + indent + i[1][1] * cell_size))
@@ -89,30 +92,27 @@ board = Board(8, 8)
 running = True
 arrow = pygame.image.load("arrow.png")
 pygame.mouse.set_visible(False)
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            x, y = event.pos
-            print(board.check(x, y))
-            if board.check(x, y) is not None:
-                if board.check(x, y)[0] and turtle_active:
-                    arrow = pygame.image.load("arrow.png")
-                    turtle_active = False
-                    check = board.check(x, y)
-                    if check[0] == True and check[1][1] > 3:
-                        board_units.append(['cyber_Turtle_frendly.png', check[1]])
-                if board.check(x, y) == 'turtle':
-                    arrow = pygame.image.load('cyber_Turtle_frendly.png')
-                    turtle_active = True
-
-
-
-
+        # if event.type == pygame.MOUSEBUTTONDOWN:
+            # x, y = event.pos
+            # print(board.check(x, y))
+            # if board.check(x, y) is not None:
+                # if board.check(x, y)[0] and turtle_active:
+                    # arrow = pygame.image.load("arrow.png")
+                    # turtle_active = False
+                    # check = board.check(x, y)
+                    # if check[0] == True and check[1][1] > 3:
+                        # board_units.append(['cyber_Turtle_frendly.png', check[1]])
+                # if board.check(x, y) == 'turtle':
+                    # arrow = pygame.image.load('cyber_Turtle_frendly.png')
+                    # turtle_active = True
 
     screen.fill((163, 110, 255))
-    board.render()  # показываем дисплей
+    board.render()
     board.update()
     if pygame.mouse.get_focused():
         x, y = pygame.mouse.get_pos()

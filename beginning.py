ca011b046ec65f56ec12dfb63873e3d548447c4d
Надя <nad20060628@gmail.com>
2022-01-04 14:03:45 +0300

    менюха

--- beginning.py
+++ beginning.py
@@ -4,45 +4,49 @@ import pygame
 pygame.init()
 
 
-class Menu():
+class Menu:
     def __init__(self):
         screen = pygame.display.set_mode((500, 400))
         self.screen = screen
         pygame.display.set_caption("menu")
         font = pygame.font.SysFont('Arial', 35)
         # создание кнопок меню
+        # кнопка старт
         start_button = pygame.draw.rect(self.screen, (57, 255, 20), (20, 40, 150, 50))
         self.start_button = start_button
         self.screen.blit(font.render('START', True, (0, 0, 0)), (50, 50))
-
+        # кнопка правила
         continue_button = pygame.draw.rect(self.screen, (57, 255, 20), (20, 130, 150, 50))
         self.continue_button = continue_button
         self.screen.blit(font.render('RULES', True, (0, 0, 0)), (50, 140))
-
+        # кнопка выход
         quit_button = pygame.draw.rect(self.screen, (57, 255, 20), (20, 220, 150, 50))
         self.quit_button = quit_button
         self.screen.blit(font.render('EXIT', True, (0, 0, 0)), (50, 230))
-
         pygame.display.update()
         pygame.display.flip()
         self.moving()
 
+    # функция создания меню
     def moving(self):
-        menuativo = True
-        while menuativo:
+        while True:
             for event in pygame.event.get():
                 if event.type == pygame.MOUSEMOTION:
                     if 170 >= pygame.mouse.get_pos()[0] >= 20 and 90 >= pygame.mouse.get_pos()[1] >= 40:
                         pass
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if 170 >= pygame.mouse.get_pos()[0] >= 20 and 90 >= pygame.mouse.get_pos()[1] >= 40:
+                        # запускаем функцию старт
                         self.start()
                     if 170 >= pygame.mouse.get_pos()[0] >= 20 and 180 >= pygame.mouse.get_pos()[1] >= 130:
+                        # запускаем функцию правила
                         self.rules()
                     if 170 >= pygame.mouse.get_pos()[0] >= 20 and 270 >= pygame.mouse.get_pos()[1] >= 220:
-                        pygame.quit()
+                        # выход из игры
+                        exit()
 
     def rules(self):
+        # выводим правила по запросу из бд
         self.screen = pygame.display.set_mode((500, 400))
         self.screen.fill('black')
         pygame.display.set_caption("Rules")
@@ -50,10 +54,10 @@ class Menu():
         pygame.display.flip()
         while pygame.event.wait().type != pygame.QUIT:
             pygame.display.update()
-        pygame.quit()
+        exit()
 
     def start(self):
-        pygame.quit()
+        exit()
 
 
 a = Menu()

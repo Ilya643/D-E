eb4f99a104d73d956af73ab666b6746aa5a3b222
Надя <nad20060628@gmail.com>
2022-01-06 21:44:01 +0300

    продвинутая менюха

--- beginning.py
+++ beginning.py
@@ -1,9 +1,10 @@
 import pygame
 
 pygame.init()
+
+
+# меню игры D&E
 # создание самого первого окна меню
-
-
 class Menu:
     def __init__(self):
         screen = pygame.display.set_mode((640, 480))
@@ -74,13 +75,15 @@ class Menu:
             main()
         exit()
 
+
 # создание элементов для 2 меню
 screen = pygame.display.set_mode((640, 480))
 color_inactive = pygame.Color((0, 255, 0))
 color_active = pygame.Color('dodgerblue2')
 font = pygame.font.Font(None, 32)
 
-# класс 2 меню
+
+# класс 2 меню, поле ввода время игры
 class InputBox:
     def __init__(self, x, y, w, h, text=''):
         self.rect = pygame.Rect(x, y, w, h)
@@ -89,6 +92,7 @@ class InputBox:
         self.txt_surface = font.render(text, True, self.color)
         self.active = False
 
+    # функция для создания поля ввода
     def handle_event(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN:
             # If the user clicked on the input_box rect.
@@ -116,7 +120,6 @@ class InputBox:
                 # Re-render the text.
                 self.txt_surface = font.render(self.text, True, self.color)
 
-
     def update(self):
         # Resize the box if the text is too long.
         width = max(70, self.txt_surface.get_width() + 10)
@@ -124,23 +127,23 @@ class InputBox:
 
     def draw(self, screen):
         screen.blit(self.txt_surface, (self.rect.x + 5, self.rect.y + 5))
-        screen.blit(font.render('Время игры:', True, (57, 255, 20)), (30, 415))
-        screen.blit(font.render('Время хода:', True, (57, 255, 20)), (30, 515))
+        screen.blit(font.render('Время игры (в минутах):', True, (57, 255, 20)), (30, 415))
+        screen.blit(font.render('Выберите уровень игры:', True, (57, 255, 20)), (20, 15))
         pygame.draw.rect(screen, self.color, self.rect, 2)
 
 
 def main():
     clock = pygame.time.Clock()
     input_box1 = InputBox(300, 400, 140, 50)
-    input_box2 = InputBox(300, 500, 140, 50)
     # список полей ввода
-    input_boxes = [input_box1, input_box2]
+    input_boxes = [input_box1]
     done = False
     while not done:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
             for box in input_boxes:
+
                 box.handle_event(event)
         for box in input_boxes:
             box.update()

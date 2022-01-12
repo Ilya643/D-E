69fd6ffd120049760bbe92e0eba1cf29c3b30026
Надя <nad20060628@gmail.com>
2022-01-12 18:27:05 +0300

    продвинутая менюха твердая проверка на ввод времени, выбор скина

--- beginning.py
+++ beginning.py
@@ -208,6 +208,10 @@ def mn():
     active = False
     text = '10'
     time_hod = 'Error'
+    # цвета кнопок выбора скина
+    turtole_color = pygame.Color('Silver')
+    crab_color = pygame.Color('Silver')
+    octupos_color = pygame.Color('Silver')
     pygame.display.flip()
     while True:
         for event in pygame.event.get():
@@ -258,18 +262,25 @@ def mn():
         # скин краба
         crab = pygame.image.load('img_crab.bmp')
         crab.set_colorkey((255, 255, 255))
-        crab_rect = crab.get_rect(center=(250 // 2, 720 // 2))
+        crab_rect = crab.get_rect(center=(250 // 2, 790 // 2))
         screen.blit(crab, crab_rect)
+        # кнопка названия краба
+        pygame.draw.rect(screen, (57, 255, 20), (30, 240, 140, 30))
         # скин осьминога
         octopus = pygame.image.load('img_Octopus.bmp')
         octopus.set_colorkey((255, 255, 255))
-        octopus_rect = octopus.get_rect(center=(550 // 2, 760 // 2))
+        octopus_rect = octopus.get_rect(center=(550 // 2, 820 // 2))
         screen.blit(octopus, octopus_rect)
+        # кнопка названия осьминога
+        pygame.draw.rect(screen, (57, 255, 20), (230, 240, 140, 30))
         # скин черепахи
         turtole2 = pygame.image.load('img_turtel2.bmp')
         turtole2.set_colorkey((255, 255, 255))
-        turtole2_rect = turtole2.get_rect(center=(950 // 2, 760 // 2))
+        turtole2_rect = turtole2.get_rect(center=(950 // 2, 820 // 2))
         screen.blit(turtole2, turtole2_rect)
+        # кнопка названия черепахи
+        pygame.draw.rect(screen, (57, 255, 20), (460, 240, 140, 30))
+
         # прямоугольник для кнопки возврата
         pygame.draw.rect(screen, (57, 255, 20), (20, 470, 140, 70))
         # прямоугольник для кнопки начать игру
